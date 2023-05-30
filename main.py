8a282db48723f253e3a72b2505b1ede5fb4785d6
linh <khanhlinhvng>
2023-05-30 15:54:10 +0700

    2 cameras tasks

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
 from task1 import *
 from task2 import *
 from scheduler import *
-import time
+import time 
+import cv2 
 
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
+# task1 = Task1()
+# task2 = Task2()
+
+# schedule.SCH_Add_Task(task1.Task1_Run, 1000, 50000)
+# schedule.SCH_Add_Task(task2.Task2_Run, 2000, 60000)
+
+
+# while True:
+#     schedule.SCH_Update()
+#     schedule.SCH_Dispatch_Tasks()
+
+# print("Hello LAB2")
+# import time
+# from scheduler import *
+# from task1 import *
+# from task2 import *
+# scheduler = Scheduler()
+# scheduler.SCH_Init()
+
+# task1 = Task1()
+# task2 = Task2()
+
+# scheduler.SCH_Add_Task(task1.Task1_Run, 1000,2000)
+# scheduler.SCH_Add_Task(task2.Task2_Run, 2000,4000)
+
+# while True:
+#     scheduler.SCH_Update()
+#     scheduler.SCH_Dispatch_Tasks()
+#     time.sleep(1)
+
 class TimerTask: 
     def __init__(self):
-        pass
+        self.timer = 0
 
     def run(self):
-        print("At time: {self.timer}")
+        print(f"At time: {self.timer}")
         self.timer += 1
 
     timer = 0
 
-task1 = Task1()
-task2 = Task2()
+task1a = Task1(0)
+task1b = Task1(1)
 timer_task = TimerTask()
 
 scheduler.SCH_Add_Task(task1.Task1_Run, 1000, 2000)
-scheduler.SCH_Add_Task(task2.Task2_Run, 1000, 4000)
-scheduler.SCH_Add_Task(timer_task.run, 0,1000)
+scheduler.SCH_Add_Task(task1b.Task1_Run, 1000, 4000)
+scheduler.SCH_Add_Task(timer_task.run, 0, 1000)
 
 while True:
     scheduler.SCH_Update()
     scheduler.SCH_Dispatch_Tasks()
-    time.sleep(1)
+    time.sleep(0.1)
+    keyboard_input = cv2.waitKey(1)
+    if keyboard_input == 27:
+        break
+
+task1b.camera.release()
+task1a.camera.release()
+cv2.destroyAllWindows()
--- task1.py
+++ task1.py
@@ -1,11 +1,14 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
-import cv2  # Install opencv-python
-import numpy as np
+from keras.models import load_model
+import cv2
+import numpy as np 
 
 
 class Task1:
-    def __init__(self):
-        print("Init task 1")
+    def __init__(self, index):
+        self.index = index
+        self.stopped = False 
+        self.is_activated = False
+        print("Init task 1", index)
 
         np.set_printoptions(suppress=True)
         # Disable scientific notation for clarity
@@ -17,35 +20,56 @@ class Task1:
         self.class_names = open("labels.txt", "r").readlines()
 
         # CAMERA can be 0 or 1 based on default camera of your computer
-        self.camera = cv2.VideoCapture(0)
+        self.camera = cv2.VideoCapture(index)
+
+        # Add a variable to track the number of consecutive failed attemps
+        self.failed_attempts = 0 
+        self.max_failed_attempts = 3 
 
         return
 
     def Task1_Run(self):
-        print("Task 1 is actived")
+        if not self.is_activated:
+            print("2 camera task, camera", self.index, "is activated")
+        self.is_activated = True
 
-        # Grab the webcamera's image.
-        ret, image = self.camera.read()
+        while not self.stopped:
+            # Grab the webcamera's image.
+            ret, image = self.camera.read()
+       
+            if not ret:
+                self.failed_attempts += 1
+                print("Failed to capture image from camera", self.index)
+                if self.failed_attempts >= self.max_failed_attempts:
+                    print("Maximum number of consecutive failed attempts reached. Please try to connect cameras again")
+                    self.stopped = True
+                continue
+            
+            #Reset failed attempts counter
+            self.failed_attempts = 0 
+            
+            # Resize the raw image into (224-height,224-width) pixels
+            image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
-        # Resize the raw image into (224-height,224-width) pixels
-        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+            # Show the image in a window
+            cv2.imshow("Webcam Image", image)
 
-        # Show the image in a window
-        cv2.imshow("Webcam Image", image)
+            # Make the image a numpy array and reshape it to the models input shape.
+            image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
-        # Make the image a numpy array and reshape it to the models input shape.
-        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+            # Normalize the image array
+            image = (image / 127.5) - 1
 
-        # Normalize the image array
-        image = (image / 127.5) - 1
+            # Predicts the model
+            prediction = self.model.predict(image)
+            index = np.argmax(prediction)
+            class_name = self.class_names[index]
+            confidence_score = prediction[0][index]
 
-        # Predicts the model
-        prediction = self.model.predict(image)
-        index = np.argmax(prediction)
-        class_name = self.class_names[index]
-        confidence_score = prediction[0][index]
+            # Print prediction and confidence score
+            print("Class:", class_name[2:], end="")
+            print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+            cv2.waitKey(1)
 
-        # Print prediction and confidence score
-        print("Class:", class_name[2:], end="")
-        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-        cv2.waitKey(1)
+task1 = Task1(1) 
+task1.Task1_Run()
